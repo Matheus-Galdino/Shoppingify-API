3c2db22594f102c106ee1e0fb9c75abeea01cbc4
Matheus Carvalho <matheus10277@gmail.com>
2021-06-02 18:55:48 -0300

    Adding constructors to classes

--- Models/Item.cs
+++ Models/Item.cs
@@ -6,6 +6,23 @@
         public string Name { get; set; }
         public string Note { get; set; }
         public string ImageUrl { get; set; }
-        public Category Category { get; set; }
+        public Category Category { get; set; }       
+
+        public Item(string name, string note, string imageUrl, Category category)
+        {
+            Name = name;
+            Note = note;
+            ImageUrl = imageUrl;
+            Category = category;
+        }
+
+        public Item(int id, string name, string note, string imageUrl, Category category)
+        {
+            Id = id;
+            Name = name;
+            Note = note;
+            ImageUrl = imageUrl;
+            Category = category;
+        }
     }
 }
