990f5842fd271fbc3471f75b04704dacaac03014
Matheus Carvalho <matheus10277@gmail.com>
2021-08-05 13:17:37 -0300

    Migrations

---
+++ Migrations/20210719050149_ListItems.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingifyAPI.Migrations
+{
+    public partial class ListItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingListItem",
+                columns: table => new
+                {
+                    ShoppingListId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingListItem", x => new { x.ItemId, x.ShoppingListId });
+                    table.ForeignKey(
+                        name: "FK_ShoppingListItem_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShoppingListItem_ShoppingLists_ShoppingListId",
+                        column: x => x.ShoppingListId,
+                        principalTable: "ShoppingLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingListItem_ShoppingListId",
+                table: "ShoppingListItem",
+                column: "ShoppingListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingListItem");
+        }
+    }
+}
