fa35da42505f399b7a25b1effd0b2088f26f86a9
Matheus Carvalho <matheus10277@gmail.com>
2021-08-13 16:58:37 -0300

    Adding stat class

---
+++ Models/Stat.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingifyAPI.Models
+{
+    public class Stat
+    {
+        public string Key { get; set; }
+        public int Amount { get; set; }
+        public decimal Percentage { get; set; }        
+    }
+}
